e24fee603ef3da3e0bda8d28a6063b5138484b89
Khalil Honsali <k.honsali@gmail.com>
2016-11-24 01:39:06 +0100

    minor

--- sw.js
+++ sw.js
@@ -14,3 +14,18 @@ this.addEventListener('install', function(event) {
 this.addEventListener('activate', function(event) {
 	 console.log("sw activate event");
 });
+
+this.addEventListener('fetch', function(event) {
+	  event.respondWith(
+	    caches.match(event.request).then(function(resp) {
+	      return resp || fetch(event.request).then(function(response) {
+	        caches.open('v1').then(function(cache) {
+	          cache.put(event.request, response.clone());
+	        });
+	        return response;
+	      });
+	    }).catch(function() {
+	      return caches.match('/test-sw/img/logo.png');
+	    })
+	  );
+	});
