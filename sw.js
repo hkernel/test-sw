cd3fbbda8a79fdba8c141c8240d068ebde3d0c62
Khalil Honsali <k.honsali@gmail.com>
2016-11-24 01:31:19 +0100

    minor

--- sw.js
+++ sw.js
@@ -1,10 +1,15 @@
-
-
 this.addEventListener('install', function(event) {
-	  event.waitUntil(
-	    console.log("service worker installing");
-	  );
-	});
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        '/hkernel/test-sw/master/',
+        '/hkernel/test-sw/master/index.html',
+        '/hkernel/test-sw/master/app.js',
+        '/hkernel/test-sw/master/img/logo.png'
+      ]);
+    })
+  );
+});
 
 this.addEventListener('activate', function(event) {
 	 console.log("sw activate event");
